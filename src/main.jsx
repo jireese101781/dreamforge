92a911ea8ebc0a300a2e6a5b679bfa56364835bb
jireese101781 <jireesebarris5@gmail.com>
2026-09-09 17:03:23 -0500

    Load organized Story Creator workspace

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import './coverCropper.js'
 import './coverDisplayFix.js'
 import './characterSystem.js'
+import './storyCreatorOrganizerV2.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
