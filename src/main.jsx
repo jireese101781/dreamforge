ffb0125823b9732c7274d20558b7f223462d973b
jireese101781 <jireesebarris5@gmail.com>
2026-09-09 16:42:24 -0500

    Load character assignment system

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './coverCropper.js'
 import './coverDisplayFix.js'
+import './characterSystem.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
