468b7dcd81c8815ff679f8a9010370dcb00db052
jireese101781 <jireesebarris5@gmail.com>
2026-09-09 17:11:39 -0500

    Load Story Creator UI sizing fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './coverCropper.js'
 import './coverDisplayFix.js'
 import './characterSystem.js'
 import './storyCreatorOrganizerV2.js'
+import './storyCreatorUiFix.js'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
